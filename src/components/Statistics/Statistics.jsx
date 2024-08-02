f09845fc45e9d3b39e92a939f387893a95c07c82
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-08-02 15:36:43 +0300

    add Stattictics and Heading styles

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,25 @@
+import { FaRegThumbsUp } from "react-icons/fa";
+import { MdPeople, MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { GiTreeDoor } from "react-icons/gi";
+
 import { StatisticsItem } from "../StatisticsItem/StatisticsItem";
+import s from "./Statistics.module.css";
+
+const icons = [
+  <FaRegThumbsUp key="FaRegThumbsUp" />,
+  <MdPeople key="MdPeople" />,
+  <MdOutlineProductionQuantityLimits key="MdOutlineProductionQuantityLimits" />,
+  <GiTreeDoor key="GiTreeDoor" />,
+];
 
 export const Statistics = ({ title, stats }) => {
   return (
     <>
-      {title && <h2>{title}</h2>}
-      <ul>
-        {stats.map((stat) => (
-          <li key={stat.id}>
-            <StatisticsItem {...stat} />
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.list}>
+        {stats.map((stat, index) => (
+          <li className={s.item} key={stat.id}>
+            <StatisticsItem {...stat} icon={icons[index]} />
           </li>
         ))}
       </ul>
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,8 +1,13 @@
-export const StatisticsItem = ({ title, total }) => {
+import { IconContext } from "react-icons";
+import s from "./StatisticsItem.module.css";
+export const StatisticsItem = ({ title, total, icon }) => {
   return (
     <>
-      <h3>{title}</h3>
-      <span>{total}</span>
+      <IconContext.Provider value={{ color: "blue", size: "30px" }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={s.counter}>{total}</span>
+      <p className={s.text}>{title}</p>
     </>
   );
 };
