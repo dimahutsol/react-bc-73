2c7d330996e3700ae340d2f0bd5a94f2e87b853a
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-08-06 14:08:18 +0200

    LocalStorage

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +9,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      components: '/src/components',
-      data: '/src/data',
-      styles: '/src/styles',
-      tabs: '/src/tabs',
+      components: "/src/components",
+      data: "/src/data",
+      styles: "/src/styles",
+      tabs: "/src/tabs",
+      hooks: "/src/hooks",
     },
   },
 });
