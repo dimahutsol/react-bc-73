369ae613ed40e8105ee9e0601f8d2304d509bbd8
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-08-15 15:44:44 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,31 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Props from "./pages/Props/Props";
-import Points from "./pages/Points/Points";
-import Quize from "./pages/Quize/Quize";
-import Todos from "./pages/Todos/Todos";
-import Photos from "./pages/Photos/Photos";
-import HomePage from "./pages/HomePage/HomePage";
-import { Header } from "./components";
-import Countries from "./pages/Countries/Countries";
-import SearchCountries from "./pages/SearchCountries/SearchCounties";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Props from './pages/Props/Props';
+import Points from './pages/Points/Points';
+import Quize from './pages/Quize/Quize';
+import Todos from './pages/Todos/Todos';
+import Photos from './pages/Photos/Photos';
+import HomePage from './pages/HomePage/HomePage';
+import { Header } from './components';
+import Countries from './pages/Countries/Countries';
+import SearchCountries from './pages/SearchCountries/SearchCounties';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/photos" element={<Photos />} />
-        <Route path="/quize" element={<Quize />} />
-        <Route path="/points" element={<Points />} />
-        <Route path="/props" element={<Props />} />
-        <Route path="/todos" element={<Todos />} />
-        <Route path="/countries" element={<Countries />} />
-        <Route path="/searchCountries" element={<SearchCountries />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/photos' element={<Photos />} />
+				<Route path='/quize' element={<Quize />} />
+				<Route path='/points' element={<Points />} />
+				<Route path='/props' element={<Props />} />
+				<Route path='/todos' element={<Todos />} />
+				<Route path='/countries' element={<Countries />} />
+				<Route path='/searchCountries' element={<SearchCountries />} />
+				<Route path='*' element={<Navigate to='/' />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
-{
-  /* <Tabs>
-      <TabList>
-        <Tab>Photos</Tab>
-        <Tab>Todos</Tab>
-        <Tab>Quize</Tab>
-        <Tab>Points</Tab>
-        <Tab>Props</Tab>
-      </TabList>
-
-      <TabPanel>
-        <Photos />
-      </TabPanel>
-      <TabPanel>
-        <Todos />
-      </TabPanel>
-      <TabPanel>
-        <Quize />
-      </TabPanel>
-      <TabPanel>
-        <Points />
-      </TabPanel>
-      <TabPanel>
-        <Props />
-      </TabPanel>
-    </Tabs> */
-}
