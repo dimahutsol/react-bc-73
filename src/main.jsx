e3baaff4523ab4e2dbaefc8ac8154af986b325d7
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-08-15 15:40:58 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import "modern-normalize/modern-normalize.css";
-import { Toaster } from "react-hot-toast";
-import { BrowserRouter } from "react-router-dom";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
-  </React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import App from './App.jsx';
+import './index.css';
+import 'modern-normalize/modern-normalize.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+		<Toaster />
+	</React.StrictMode>
 );
