3a21cf998c02728a51782a97ed7003587af8b1d2
Dima Hutsol <35511017+dimahutsol@users.noreply.github.com>
2024-08-16 12:08:39 +0100

    Update pexelsAPI.js

--- src/services/pexelsAPI.js
+++ src/services/pexelsAPI.js
@@ -5,6 +5,16 @@
 // );
 
 export const fetchImages = async (query, page) => {
+	return await fetch(
+		`https://api.pexels.com/v1/search?query=${query}&per_page=20&page=${page}`,
+		{
+			headers: {
+				Authorization:
+					'563492ad6f9170000100000108dc2880626e4436b3634ce1cf6b4d74',
+			},
+		}
+	).then(res => res.json());
+
 	// return client.photos.search({
 	//   query,
 	//   per_page: 20,
